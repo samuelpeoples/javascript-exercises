7faf20cc7afb4de16cfb1446bcc7d4296b3adb7c
samuelpeoples <peoples.sam@gmail.com>
2025-02-02 16:51:36 +1100

    Completed Lesson 2, Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(entry, repeatsNum) {
+    if (repeatsNum < 0) { return "ERROR" }
+    let string = ''
+    for (let i = 0; i < repeatsNum; i++) {
+        string += entry;
+    }
+    return string
 };
 
 // Do not edit below this line
