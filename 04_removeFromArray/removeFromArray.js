ba1b40500d80fe21bdbdb443c7d2a67a1c2df2c1
samuelpeoples <peoples.sam@gmail.com>
2025-02-03 22:36:33 +1100

    Completed Lesson 4, Remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valToRemove) {
+    //return array.filter(val => !valToRemove.includes(val));
+
+    const newArray = [];
+    array.forEach(element => {
+        if(!valToRemove.includes(element)){ newArray.push(element) };
+    });
+    return newArray;
 };
 
 // Do not edit below this line
