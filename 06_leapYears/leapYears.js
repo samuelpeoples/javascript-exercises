afae26d4c54c5a52522a1ab679f49a8f1b03aef3
samuelpeoples <peoples.sam@gmail.com>
2025-02-05 14:28:54 +1100

    Completed Lesson 6, Leap Years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    let isStandard = year % 4 === 0;
+    let isCentury = year % 100 === 0;
+    let isCenturyLeap = year % 400 === 0;
 
+    return isStandard && (!isCentury || isCenturyLeap);
 };
 
 // Do not edit below this line
