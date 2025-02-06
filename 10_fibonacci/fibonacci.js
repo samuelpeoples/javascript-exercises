e5880ea7324b374e2c9e160a86131547c99052b9
samuelpeoples <peoples.sam@gmail.com>
2025-02-06 20:57:47 +1100

    Completed Lesson 10, Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (arg) {
+	arg = parseInt(arg);
 
+	if (arg < 0) {
+		return "OOPS";
+	}
+	if (arg <= 1) {
+		return arg;
+	}
+
+	return fibonacci(arg - 1) + fibonacci(arg - 2);
 };
 
 // Do not edit below this line
