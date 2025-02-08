014897ef168a0cb090bd9e339ff1bf77fbb25662
samuelpeoples <peoples.sam@gmail.com>
2025-02-08 17:10:15 +1100

    Completed Leson 12, Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+function calculateAge(birthYear, deathYear) {
+	if (!deathYear) {
+		deathYear = new Date().getFullYear();
+	}
+	return deathYear - birthYear;
+}
 
+const findTheOldest = function (people) {
+	/**
+	 * Get birth years
+	 * If year of death is defined, age == Birth Year - Death Year
+	 * If year of death is undefined, age == Birth Year - Current Year
+	 */
+
+	return people.reduce((oldest, current) => {
+		const oldestAge = calculateAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const currentAge = calculateAge(current.yearOfBirth, current.yearOfDeath);
+		return currentAge > oldestAge ? current: oldest;
+	});
+
+	// let oldest;
+	// people.forEach((person) => {
+	// 	if (oldest == null) {
+	// 		oldest = person;
+	// 	}
+	// 	if (calculateAge(person.yearOfBirth, person.yearOfDeath) > calculateAge(oldest.yearOfBirth, oldest.yearOfDeath)) {	
+    //         oldest = person; }
+	// });
+
+	// return oldest;
 };
 
 // Do not edit below this line
