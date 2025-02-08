c37334708543be725e16196b364aa7b06c46c5a4
samuelpeoples <peoples.sam@gmail.com>
2025-02-08 15:47:16 +1100

    Completed Lesson 11, Get the Titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+	let booksTitles = [];
+	for (let i = 0; i < array.length; i++) {
+		booksTitles.push(array[i].title);
+	}
+	console.log(booksTitles);
+	return booksTitles;
+	/*return array.map((item) => item.title);*/
 };
 
 // Do not edit below this line
